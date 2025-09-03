65a57c5efcd0c1f526a7a5629a9b1af9e37e7353
John <johnnjau202@gmail.com>
2025-09-03 17:17:20 +0300

    concatenation

--- 0x001-JS/main.js
+++ 0x001-JS/main.js
@@ -2,10 +2,6 @@
 
 const name = 'John';
 const age = 30;
-const rating = 4.5;
-const isCool = true;
-const x = null;
-const y = undefined;
-let z;
 
-console.log(typeof name);
+//Concatenation
+console.log('My name is ' + name + ' and I am ' + age);
