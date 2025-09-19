785954214639f4fff6a182ce77eb2383f388d297
John <johnnjau202@gmail.com>
2025-09-19 14:38:23 +0300

    triple equal sign test string vs number

--- 0x001-JS/main.js
+++ 0x001-JS/main.js
@@ -1,14 +1,5 @@
-const todos = [
-  { id: 1, title: 'Buy groceries', completed: false },
-  { id: 2, title: 'Walk the dog', completed: true },
-  { id: 3, title: 'Read a book', completed: false },
-];
+const x = 10;
 
-// forEach, map, filter
-const todoCompleted = todos.filter(function(todo) {
-  return todo.completed === false;
-}).map(function(todo) { 
-  return todo.title;
-})
-
-console.log(todoCompleted);
+if (x === 10) {
+  console.log('x is 10'); // This will log because of type coercion
+}
